0a78af1daa69665324021b364f68cfb642b590ff
Bin2Chen <binbin.chen@thoughtworks.com>
2024-05-21 22:20:31 +0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -8,29 +8,4 @@ variable "aws_assume_role" {
 }
 
 variable "cluster_name" {}
-variable "cluster_version" {}
-variable "cluster_enabled_log_types" {}
-variable "cluster_log_retention" {}
-variable "vpc_cni_version" {}
-variable "coredns_version" {}
-variable "kube_proxy_version" {}
-variable "aws_ebs_csi_version" {}
 variable "alert_channel" {}
-variable "kube_proxy_version_test" {}
-
-variable "default_node_group_name" {}
-variable "default_node_group_ami_type" {}
-variable "default_node_group_platform" {}
-variable "default_node_group_min_size" {}
-variable "default_node_group_max_size" {}
-variable "default_node_group_desired_size" {}
-variable "default_node_group_disk_size" {}
-variable "default_node_group_capacity_type" {}
-variable "default_node_group_instance_types" {}
-
-variable "oidc_client_id" {
-  sensitive = true
-}
-variable "oidc_groups_claim" {}
-variable "oidc_identity_provider_config_name" {}
-variable "oidc_issuer_url" {}
